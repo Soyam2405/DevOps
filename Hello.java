42f38aa03ebe178614e4d772bd5feb52b6c4ccf6
Soyam Kumar Gupta <120269736+Soyam2405@users.noreply.github.com>
2024-08-19 21:51:39 +0530

    Updated

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 public static void main(String[] args){
-System.out.println("Hello Soyam");
-  System.out.println("Sum of two digits is "+(2+3));
+//System.out.println("Hello Soyam");
+  //System.out.println("Sum of two digits is "+(2+3));
+  System.out.println("Hey! I am building the project ");
 }
 }
